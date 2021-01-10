1002921edbbe95a7f62f3cbc41cbb6a742b80b77
YoannP-08 <yoann.pons08@gmail.com>
2021-01-10 20:03:10 +0100

    [ADD] authentication system - signin/sigup done with auth0 via auth0-nextjs library

--- imdb-movies/next.config.js
+++ imdb-movies/next.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     env: {
-        MONGO_URI: "mongodb+srv://imdb:2houiIJJdAGlqtV0@cluster0.sa365.mongodb.net/rottentomato?retryWrites=true&w=majority"
+        MONGO_URI: "mongodb+srv://imdb:2houiIJJdAGlqtV0@cluster0.sa365.mongodb.net/rottentomato?retryWrites=true&w=majority",
+        AUTH0_DOMAIN: "imdb-movies.eu.auth0.com",
+        AUTH0_CLIENT_SECRET: "ZeOqjRrqPRfGxGo6Ka5QUq1KnUS-svQGtcBad80jM6AuzPT7uXJp1EV_KA5z4FBa",
+        AUTH0_CLIENT_ID: "O9FV1WHMedA0w7AWna6JUnn6oxrv0Q8S",
+        COOKIE_SECRET: "qwertyuioplkjhgfdsazxcvbnm0987612345"
     }
 }
