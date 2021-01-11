1c04c61821ed794a2408fb9065544c40fc5f5a5d
Bigo <jbsilvestre@hotmail.fr>
2021-01-11 22:25:43 +0100

    user movies interface

--- imdb-movies/components/Navbar.js
+++ imdb-movies/components/Navbar.js
@@ -5,23 +5,23 @@ const Navbar = ({ user }) => {
     return(
     <nav>
         <div className="flex justify-between items-center bg-gray-900 w-full h-16 px-4">
-            <div className="text-2xl font-bold text-yellow-300 md:text-3xl">
+            <div className="text-2xl font-bold text-yellow-300 md:text-3xl font-codystar">
                 <a href="#">IMDB Movies</a>
             </div>
             <div className="flex">
                 {user.props.user ? (
                     <a
                         href="/api/auth/logout"
-                        className="rounded bg-yellow-300 text-white py-2 px-4 hover:bg-gray-900 hover:text-yellow-300 border border-4 border-yellow-300"
+                        className="rounded bg-yellow-300 text-gray-900 py-2 px-4 hover:bg-gray-900 hover:text-yellow-300 border border-4 border-yellow-300"
                     >
                         Logout
                     </a>
                 ) : (
                     <a
                         href="/api/auth/login"
-                        className="rounded bg-yellow-300 text-white py-2 px-4 hover:bg-gray-900 hover:text-yellow-300 border-2 border-yellow-300"
+                        className="rounded bg-yellow-300 text-gray-900 py-2 px-4 hover:bg-gray-900 hover:text-yellow-300 border-2 border-yellow-300"
                     >
-                        SignIn / SignUp
+                        Sign in / Sign up
                     </a>
                 )}
             </div>
---
+++ imdb-movies/components/movies/MoviesList.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import MoviePoster from "./MoviePoster";
+
+export class MoviesList extends Component {
+  state = {
+    movies: [],
+  };
+
+  fetchMovies = async () => {
+    const res = await fetch("http://localhost:3000/api/movies");
+    const movies = await res.json();
+    this.setState({ movies: movies.data });
+  };
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className="font-fira my-2 text-xl text-yellow-300">Showcased movies</h1>
+        <div className="flex flex-wrap mb-5">
+          {this.state.movies.map((movie) => (
+            <MoviePoster movie={movie} key={movie._id}></MoviePoster>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MoviesList;
