356b61b578225f28ec286dfa14aafe472b20b5ef
YoannP-08 <yoann.pons08@gmail.com>
2021-01-14 14:57:01 +0100

    [ADD] user auth0 props

--- imdb-movies/pages/admin/movies.js
+++ imdb-movies/pages/admin/movies.js
@@ -1,8 +1,9 @@
 import styles from "../../styles/Home.module.css";
 import MoviesSearch from "../../components/movies/MoviesSearch";
 import PopularMoviesList from "../../components/movies/PopularMoviesList";
+import auth0 from '../../utils/auth0';
 
-export default function Movies() {
+export default function Movies({ user }) {
   return (
     <div className={styles.container}>
       <MoviesSearch></MoviesSearch>
@@ -10,4 +11,14 @@ export default function Movies() {
       <PopularMoviesList></PopularMoviesList>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await auth0.getSession(context.req);
+  
+  return {
+      props: {
+        user: session?.user || null,
+      },
+  };
 }
